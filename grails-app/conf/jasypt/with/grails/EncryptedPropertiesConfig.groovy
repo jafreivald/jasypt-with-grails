aafbfd3327c731db2ed32e01dbbe2d0bfb7cabc9
Josh Freivald <joshua.freivald@mutualofomaha.com>
2020-10-29 09:52:25 -0500

    fix naming and package statement

--- grails-app/conf/jasypt/with/grails/EncryptedPropertiesConfig.groovy
+++ grails-app/conf/jasypt/with/grails/EncryptedPropertiesConfig.groovy
@@ -1,7 +1,9 @@
+package jasypt.with.grails
+
 import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EncryptablePropertySource("application.yml")
-class EncryptionConfig {
+class EncryptedPropertiesConfig {
 }
