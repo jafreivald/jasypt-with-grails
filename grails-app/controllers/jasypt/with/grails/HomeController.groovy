628ac52879e5043d23d6bc839fb59db5682024da
Josh Freivald <joshua.freivald@mutualofomaha.com>
2020-10-30 11:09:15 -0500

    using grailsconfiguration aware to set encrypted property

--- grails-app/controllers/jasypt/with/grails/HomeController.groovy
+++ grails-app/controllers/jasypt/with/grails/HomeController.groovy
@@ -1,19 +1,25 @@
 package jasypt.with.grails
 
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 import org.springframework.beans.factory.annotation.Value
 
-class HomeController {
-   
+class HomeController implements GrailsConfigurationAware {
 
+    String testProp
 
-    @Value('${enc.test.plainprop}') 
+    @Value('${enc.test.plainprop}')
     String plainProp
 
     def index() {
-        render "${grailsApplication.config.enc.test.prop}"
+        render "Hello $testProp"
     }
 
     def plainProp() {
         render plainProp
+    }
+
+    void setConfiguration(Config config) {
+        testProp = config.getProperty('enc.test.prop', String)
     }
 }
